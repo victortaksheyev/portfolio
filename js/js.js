6c0a4c2289adb8f24f254865ab1fb7b48dfecfa8
victortaksheyev <victortaksheyev@gmail.com>
2019-08-07 14:05:51 -0700

    updated search bar

--- js/js.js
+++ js/js.js
@@ -1,27 +1,34 @@
+// search algo
 function search() {
     let input = document.getElementById('searchBox').value;
     input = input.toLowerCase();
-    
 
     let container = document.getElementById('homeCenter');
     let icons = container.getElementsByClassName('frame');
 
-
     for (let i = 0; i < icons.length; i++) {
         let metadataTag = icons[i].getElementsByClassName('metadata');
-        console.log(metadataTag);
-        if (metadataTag) {
-            let metadata = metadataTag.textContent;
-            console.log(metadata);
-        }
-        
-        
-        
-    }
-    // let metadata = container.getElementsByTagName('span')[0];
-    
-    // console.log(icons.length);
+        let data = metadataTag.item(0).innerHTML;
+        let words = data.split(',');
 
+        let found = false;
+        for (let j = 0; j < words.length; j++) {
+            if (words[j].indexOf(input) != -1) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            icons[i].setAttribute("style", "display: none");
+        } else {
+            icons[i].setAttribute("style", "display: inline");
+        }
+
+        if (input == "") {
+            icons[i].setAttribute("style", "display: inline");
+
+        }
+    }
 }
 
-// if input is empty, display all of the boxes
+
